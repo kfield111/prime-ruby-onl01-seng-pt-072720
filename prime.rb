4ec710c35c5292948a6cbc50e7b19a710823ee2e
Kelin Field <kfield111@hotmail.com>
2020-08-06 04:13:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(integer)
-  if integer > 1
-    range = (2..integer-1).to_a
+def prime?(int)
+  if int > 1
+    range = (2..int-1).to_a
 
     range.none? do |int_to_test|
-  	integer % int_to_test == 0
+  	int % int_to_test == 0
     end
   else
     return false
