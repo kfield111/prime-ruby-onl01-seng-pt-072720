a417e08e04cec82591dc26329c7d405da4d21b4f
Kelin Field <kfield111@hotmail.com>
2020-08-06 04:13:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  if int > 1
+    range = (2..int-1).to_a
+
+    range.none? do |int_to_test|
+  	int % int_to_test == 0
+    end
+  else
+    return false
+  end
+end
